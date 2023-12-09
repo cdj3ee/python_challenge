439eb3d6eb22b3359d7db7983cb336b848fff853
cdj3ee <cdj3ee@gmail.com>
2023-12-08 22:18:45 -0600

    PyBank csv read and headers

---
+++ PyBank/main.py
@@ -0,0 +1,13 @@
+import os
+import csv
+import pandas as pd
+
+csvpath = os.path.join('C:\\Users\\cdj3e\\vu_bootcamp\\Python\\Module_3_Challenge\\python_challenge\\PyBank\\resources\\budget_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+
